16948189cf9a4adf5874d76c9a95d339ed7b86a5
shramadeep <109851887+shramadeepd@users.noreply.github.com>
2024-08-04 17:51:27 +0000

    updated

--- routes.py
+++ routes.py
@@ -1,21 +1,21 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash, session
-#from models import db, User, Category, Product, Cart, Transaction, Order
+from models import *
 from werkzeug.security import generate_password_hash, check_password_hash
+from app import app
+
 from functools import wraps
 from datetime import datetime
 
-# Create a Blueprint for the routes
-routes = Blueprint('routes', __name__)
 
 # Define the routes
-@routes.route('/')
+@app.route('/')
 def index():
     return render_template('index.html')
 
-@routes.route('/login')
+@app.route('/login')
 def login():
     return render_template('login.html')
 
-@routes.route('/register')
+@app.route('/register')
 def register():
     return render_template('register.html')
