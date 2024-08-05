3a3dd7eb58219e2581df849ac66d0292fce90073
Subhra Jyoti Datta <103535913+Zendachromian@users.noreply.github.com>
2024-08-06 03:13:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,29 @@
 from flask import Flask, render_template
 from config import Config
 from database import db
+from models import *
 
 
+def startapp():
+    app = Flask(__name__)  
+    app.config.from_object(Config)
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+        # if admin exists, else create admin
+        # admin = User.query.filter_by(is_admin=True).first()
+        # if not admin:
+        #     password_hash = generate_password_hash('admin')
+        #     admin = User(username='admin', passhash=password_hash, name='Admin', is_admin=True)
+        #     db.session.add(admin)
+        #     db.session.commit()
+    return app
 
-
+app = startapp()
+from routes import *
 
 
 if __name__ == '__main__':
-    
-    app = Flask(__name__)
-    app.config.from_object(Config)
-    db.init_app(app) 
     app.run(debug=True)
 
 
-from routes import *
